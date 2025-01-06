a21f48bca2bf76c8572f0c6e960d820484c87cb1
Diana Butiakova <75562293+bt-diana@users.noreply.github.com>
2025-01-07 02:53:25 +0400

    feat: solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  MAX_LATIN_CHAR_CODE = 'Z'.charCodeAt(0);
+  MIN_LATIN_CHAR_CODE = 'A'.charCodeAt(0);
+  direct = true;
+
+  constructor(direct) {
+    this.direct = direct ?? true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw Error('Incorrect arguments!');
+    }
+
+    const keyNumers = key
+      .toUpperCase()
+      .split('')
+      .map((char) => (char.charCodeAt(0) - this.MIN_LATIN_CHAR_CODE));
+    let index = 0;
+
+    const encryptedMessage = message.toUpperCase().split('').map((char) => {
+      const code = char.charCodeAt(0);
+      
+      if (code >= this.MIN_LATIN_CHAR_CODE && code <= this.MAX_LATIN_CHAR_CODE) {
+        return String.fromCharCode(
+          this.MIN_LATIN_CHAR_CODE 
+          + (code - this.MIN_LATIN_CHAR_CODE + keyNumers.at(index++ % keyNumers.length)) 
+          % (this.MAX_LATIN_CHAR_CODE - this.MIN_LATIN_CHAR_CODE + 1)
+        );
+      }
+
+      return char;
+    });
+
+    return this.direct ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+  }
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw Error('Incorrect arguments!');
+    }
+
+    const keyNumers = key
+      .toUpperCase()
+      .split('')
+      .map((char) => (char.charCodeAt(0) - this.MIN_LATIN_CHAR_CODE));
+    let index = 0;
+
+    const message = encryptedMessage.toUpperCase().split('').map((char) => {
+      const code = char.charCodeAt(0);
+
+      if (code >= this.MIN_LATIN_CHAR_CODE && code <= this.MAX_LATIN_CHAR_CODE) {
+        return String.fromCharCode(
+          this.MIN_LATIN_CHAR_CODE 
+          + (this.MAX_LATIN_CHAR_CODE - this.MIN_LATIN_CHAR_CODE + 1 + code - this.MIN_LATIN_CHAR_CODE - keyNumers.at(index++ % keyNumers.length)) 
+          % (this.MAX_LATIN_CHAR_CODE - this.MIN_LATIN_CHAR_CODE + 1)
+        );
+      }
+
+      return char;
+    });
+
+    return this.direct ? message.join('') : message.reverse().join('');
   }
 }
 
